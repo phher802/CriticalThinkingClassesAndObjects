6a118d4b889a8a25fc86938c30b32888bf444750
phher802 <your_email@example.com>
2020-11-11 14:42:36 -0600

    Add Motherboard class and its member variables

---
+++ ClassesAndObjectsCriticalThinking/Motherboard.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndObjectsCriticalThinking
+{
+    class Motherboard // printed circuit board - allows CPU, RAM, and all other computer hardware components to communicate w/each other
+    {
+        // member variables (HAS A)
+        public string manufacturer;
+        public CPU processor;
+        public RAM temporaryMemory;
+        public HardDrive storage;
+        public GPU graphics;
+
+        // constructor (SPAWNER)
+
+
+        // methods (CAN DO)
+
+
+
+
+    }
+}
