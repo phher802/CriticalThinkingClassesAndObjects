2dc651b0df97c214310b379d078b0ff23c36fac0
phher802 <your_email@example.com>
2020-11-11 14:59:16 -0600

    Add constructors to HardDrive class and CPU class

--- ClassesAndObjectsCriticalThinking/CPU.cs
+++ ClassesAndObjectsCriticalThinking/CPU.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ClassesAndObjectsCriticalThinking
 {
-    class CPU  //central process unit
+    class CPU  //central process unit; performs basic math, logic, controlling, input/output ops specified by instructions in program
     {
         // member variable (HAS A)
         public string manufacturer;
@@ -14,7 +14,11 @@ namespace ClassesAndObjectsCriticalThinking
 
 
         // constructor (SPAWNER)
-
+        public CPU(string manufacturer, string name)
+        {
+            this.manufacturer = manufacturer;
+            this.name = name;
+        }
 
         // methods (CAN DO)
 
