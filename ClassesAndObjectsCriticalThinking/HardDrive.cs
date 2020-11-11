e97484921016a7b9e38be4ade225e70ccbaf5471
phher802 <your_email@example.com>
2020-11-11 14:34:43 -0600

    Add HardDrive class and its member variables

---
+++ ClassesAndObjectsCriticalThinking/HardDrive.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndObjectsCriticalThinking
+{
+    class HardDrive //stores any and all content for computer; Persistent data
+    {
+        // member variables (HAS A)
+        public double totalStorage;
+        public double availableStorage;
+
+        // constructor (SPAWNER)
+
+
+        // methods (CAN DO)
+
+
+    }
+}
