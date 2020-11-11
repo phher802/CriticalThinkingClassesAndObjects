5517c892ec789dc939282800eb7560880767fd70
phher802 <your_email@example.com>
2020-11-11 14:29:50 -0600

    Add CPU class and member variables

---
+++ ClassesAndObjectsCriticalThinking/CPU.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndObjectsCriticalThinking
+{
+    class CPU  //central process unit
+    {
+        // member variable (HAS A)
+        public string manufacturer;
+        public string name;
+
+
+        // constructor (SPAWNER)
+
+
+        // methods (CAN DO)
+
+
+
+    }
+}
