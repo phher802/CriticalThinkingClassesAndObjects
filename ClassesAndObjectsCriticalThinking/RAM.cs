39cd9e9252e6da9d1abab4dfeb65f11851f67a97
phher802 <your_email@example.com>
2020-11-11 14:38:35 -0600

    Add RAM class and its member variables

---
+++ ClassesAndObjectsCriticalThinking/RAM.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndObjectsCriticalThinking
+{
+    class RAM //random-access memory: devices' short-term membory; temporarily stores/remembers everything that runs on PC e.g. windows, webit browser, game, etc.
+    {
+        // member variables (HAS A)
+        public double totalGigabytes;
+        public string brand;
+
+        // constructor (SPAWNER)
+
+
+        // method (CAN DO)
+
+
+
+    }
+}
