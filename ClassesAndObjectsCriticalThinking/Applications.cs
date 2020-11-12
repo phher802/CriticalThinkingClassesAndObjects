5babda9464ada228c9cdbc68ef4e3d5277b2b832
phher802 <your_email@example.com>
2020-11-11 20:24:37 -0600

    Create Applications class and its member variables and constructor

---
+++ ClassesAndObjectsCriticalThinking/Applications.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndObjectsCriticalThinking
+{
+    class Applications
+    {
+        // member variables (HAS A)
+        public string applicationName;
+        public string applicationType;
+        public double requireRAM;
+        public double requireStorage;
+
+        // constructor (SPAWNER)
+        public Applications()
+        {
+            applicationName = "Slack";
+            applicationType = "Messageing Service";
+            requireRAM = 4.0; //in gigaytes
+            requireStorage = 0.512; //in gigabytes
+
+        }
+
+        // methods (CAN DO)
+
+
+    }
+}
